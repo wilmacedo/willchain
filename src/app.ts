448aa7412c411b2c01d4a0593d70434e007a26a7
Wil Macedo <wil.macedo.sa@gmail.com>
2022-02-26 16:29:51 -0300

    add pk and sk to account model

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
 import Blockchain from "./models/Blockchain";
 import Transaction from "./models/Transaction";
-import * as elliptic from "elliptic";
+import Account from "./models/Account";
 
-const EC = new elliptic.ec("secp256k1");
-const sk = EC.keyFromPrivate(
-  "a77a00d1f8b835dddd6a11e7c382687575633dae736c3c8c91404a03c386c00c"
-);
-const address = sk.getPublic("hex");
+const account = new Account();
 
 let instance = new Blockchain();
 
-const tx1 = new Transaction(address, "address2", 10);
-tx1.signTransaction(sk);
+console.log(account);
+
+const tx1 = new Transaction(account.getAddress(), "address2", 10);
+tx1.signTransaction(account.getKeyPair());
 instance.addTransaction(tx1);
 
 console.log(`\n Starting the miner...`);
-instance.minePendingTransactions(address);
+instance.minePendingTransactions(account.getAddress());
 
-console.log(`\nBalance of will is ${instance.getBalanceOfAddress(address)}`);
+console.log(
+  `\nBalance of will is ${instance.getBalanceOfAddress(account.getAddress())}`
+);
--- src/models/Account.ts
+++ src/models/Account.ts
@@ -2,17 +2,25 @@ import { ec } from "elliptic";
 
 class Account {
   private addressKeyPair: ec.KeyPair;
+  private address: string;
+  private privateKey: string;
 
   constructor(addressKeyPair: ec.KeyPair = new ec("secp256k1").genKeyPair()) {
     this.addressKeyPair = addressKeyPair;
+    this.address = addressKeyPair.getPublic("hex");
+    this.privateKey = addressKeyPair.getPrivate("hex");
+  }
+
+  getKeyPair(): ec.KeyPair {
+    return this.addressKeyPair;
   }
 
   getAddress(): string {
-    return this.addressKeyPair.getPublic("hex");
+    return this.address;
   }
 
   getPrivateKey(): string {
-    return this.addressKeyPair.getPrivate("hex");
+    return this.privateKey;
   }
 }
 
